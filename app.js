802d2cfd383d28831238967c7ed011457df1ab2a
azimnorqulov <azim.norqulov@icloud.com>
2025-09-25 12:27:59 +0500

    added shopping cart

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-// document.querySelectorAll(".quantity").forEach(function (quantity) {
-//   const input = quantity.querySelector("input");
-//   const minus = quantity.querySelector(".minus");
-//   const plus = quantity.querySelector(".plus");
 
-//   plus.addEventListener("click", () => {
-//     input.value = parseInt(input.value) + 1;
-//   });
-
-//   minus.addEventListener("click", () => {
-//     let value = parseInt(input.value);
-//     if (value > 1) {
-//       input.value = value - 1;
-//     }
-//   });
-// });
